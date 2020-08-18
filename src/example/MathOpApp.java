6126233c165b5f187373e661794d83392d5afb3e
tahirih2 <tahirih2@ODERALW02063769.od.nih.gov>
2020-08-18 15:17:38 -0400

    implement addition method

--- src/example/MathOpApp.java
+++ src/example/MathOpApp.java
@@ -1,19 +1,32 @@
 package example;
 
+import java.util.Scanner;
+
 public class MathOpApp {
 	
 	/*
 	 * This a math app that execute:
-	 * Addition
-	 * Multiplication
-	 * Devision
-	 * module
-	 * output if number is even or odd
+	 * Addition-Yes
+	 * Multiplication-No
+	 * Devision-No
+	 * module-No
+	 * output if number is even or odd-No
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter first num: ");
+		double num1=scan.nextDouble();
+		System.out.println("Enter second num: ");
+		double num2=scan.nextDouble();
+		
+		System.out.println("-------Operation result-------\n");
+		System.out.println("sum: " + addition(num1,num2));
 
+	}
+
+	private static double addition(double num1, double num2) {
+		return num1+num2;
 	}
 
 }

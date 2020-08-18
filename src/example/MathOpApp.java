0806fe40d7fb4b4cf977cae0ce459ae91e8664c3
tahirih2 <tahirih2@ODERALW02063769.od.nih.gov>
2020-08-18 15:12:45 -0400

    initial commit

---
+++ src/example/MathOpApp.java
@@ -0,0 +1,19 @@
+package example;
+
+public class MathOpApp {
+	
+	/*
+	 * This a math app that execute:
+	 * Addition
+	 * Multiplication
+	 * Devision
+	 * module
+	 * output if number is even or odd
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

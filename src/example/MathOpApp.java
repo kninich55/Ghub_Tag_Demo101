ccc19daf16e905c959dd4cef5298f0a624adee31
tahirih2 <tahirih2@ODERALW02063769.od.nih.gov>
2020-08-18 16:34:16 -0400

    implement oddEven property

--- src/example/MathOpApp.java
+++ src/example/MathOpApp.java
@@ -12,7 +12,7 @@ public class MathOpApp {
 	 * module-Yes
 	 * Division-Yes [handle zero division]
 	 * module-Yes
-	 * output if number is even or odd-No
+	 * output if number is even or odd-Yes
 	 */
 
 	public static void main(String[] args) {
@@ -29,14 +29,25 @@ public class MathOpApp {
 			System.out.println("product : " + multiply(num1, num2));
 			System.out.println("division: !!ERROR!!" );
 			System.out.println("module  : " + mod(num1,num2));
+			System.out.println("Number " + num1 + " is : " + oddEven(num1));
+			System.out.println("Number " + num2 + " is : " + oddEven(num2));
 		} else {
 			System.out.println("-------Operation result-------\n");
 			System.out.println("sum: " + addition(num1, num2));
 			System.out.println("product : " + multiply(num1, num2));
 			System.out.println("division: " + division(num1, num2));
 			System.out.println("module  : " + mod(num1,num2));
+			System.out.println("Number " + num1 + " is : " + oddEven(num1));
+			System.out.println("Number " + num2 + " is : " + oddEven(num2));
 		}
 
+	}
+
+	private static String oddEven(double num1) {
+		if(num1%2==0)
+		return "Event";
+		else
+		return "Odd";
 	}
 
 	private static double mod(double num1, double num2) {

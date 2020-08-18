bd9f82c9c27b7a649aeabcd669626ece16b65833
tahirih2 <tahirih2@ODERALW02063769.od.nih.gov>
2020-08-18 15:46:32 -0400

    implemente module operation

--- src/example/MathOpApp.java
+++ src/example/MathOpApp.java
@@ -9,7 +9,7 @@ public class MathOpApp {
 	 * Addition-Yes
 	 * Multiplication-Yes
 	 * Division-Yes
-	 * module-No
+	 * module-Yes
 	 * output if number is even or odd-No
 	 */
 
@@ -24,7 +24,12 @@ public class MathOpApp {
 		System.out.println("sum: " + addition(num1,num2));
 		System.out.println("product : " + multiply(num1,num2));
 		System.out.println("division: " + division(num1,num2));
+		System.out.println("module  : " + mod(num1,num2));
 
+	}
+
+	private static double mod(double num1, double num2) {
+		return num1%num2;
 	}
 
 	private static double division(double num1, double num2) {
